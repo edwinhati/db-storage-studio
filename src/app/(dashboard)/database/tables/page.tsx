77db27fb190874f207ee5b0ea2e7165d8f7cf109
edwinhati <edwinhati@outlook.com>
2024-07-25 01:01:01 +0700

    refactor: Database tables page

--- src/app/(dashboard)/database/tables/page.tsx
+++ src/app/(dashboard)/database/tables/page.tsx
@@ -13,7 +13,7 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { cn } from "@/lib/utils";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Key } from "react";
 import {
   Eye,
   Plus,
@@ -22,6 +22,7 @@ import {
   Check,
   SquarePen,
   ArrowUpDown,
+  LoaderCircle,
   MoreHorizontal,
   ChevronsUpDown,
 } from "lucide-react";
@@ -56,7 +57,6 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import NewTableSheet from "@/components/sheets/new-table";
-import { LoaderCircle } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
 
 type Tables = {
@@ -205,63 +205,14 @@ export default function TablesPage() {
   return (
     <div className="w-full">
       <div className="flex items-center py-4 gap-2">
-        <Popover open={open} onOpenChange={setOpen}>
-          {loading ? (
-            <Button variant="outline" className="w-[200px] justify-between">
-              <LoaderCircle className="h-4 w-4 animate-spin" />
-              Loading schemas
-            </Button>
-          ) : (
-            <>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="outline"
-                  role="combobox"
-                  aria-expanded={open}
-                  className="w-[200px] justify-between"
-                >
-                  {schema
-                    ? schemas.find((item) => item.schema_name === schema)
-                        ?.schema_name
-                    : "Select schema"}
-                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-[200px] p-0">
-                <Command>
-                  <CommandInput placeholder="Search schema..." />
-                  <CommandList>
-                    <CommandEmpty>No schema found.</CommandEmpty>
-                    <CommandGroup>
-                      {schemas.map((item) => (
-                        <CommandItem
-                          key={item.schema_name}
-                          value={item.schema_name}
-                          onSelect={(currentValue) => {
-                            setSchema(
-                              currentValue === schema ? "" : currentValue
-                            );
-                            setOpen(false);
-                          }}
-                        >
-                          <Check
-                            className={cn(
-                              "mr-2 h-4 w-4",
-                              schema === item.schema_name
-                                ? "opacity-100"
-                                : "opacity-0"
-                            )}
-                          />
-                          {item.schema_name}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
-              </PopoverContent>
-            </>
-          )}
-        </Popover>
+        <SchemaCombobox
+          open={open}
+          setOpen={setOpen}
+          loading={loading}
+          schema={schema}
+          schemas={schemas}
+          setSchema={setSchema}
+        />
         <Input
           placeholder="Search for a table"
           value={
@@ -284,9 +235,109 @@ export default function TablesPage() {
         </NewTableSheet>
       </div>
       <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
+        <DatabaseTable
+          table={table}
+          loading={loading}
+          schema={schema}
+          schemas={schemas}
+        />
+      </div>
+    </div>
+  );
+}
+
+function SchemaCombobox({
+  open,
+  setOpen,
+  loading,
+  schema,
+  schemas,
+  setSchema,
+}: {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+  loading: boolean;
+  schema: string;
+  schemas: { schema_name: string }[];
+  setSchema: (value: string) => void;
+}) {
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      {loading ? (
+        <Button variant="outline" className="w-[200px] justify-between">
+          <LoaderCircle className="h-4 w-4 animate-spin" />
+          Loading schemas
+        </Button>
+      ) : (
+        <>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-[200px] justify-between"
+            >
+              {schema
+                ? schemas.find((item) => item.schema_name === schema)
+                    ?.schema_name
+                : "Select schema"}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0">
+            <Command>
+              <CommandInput placeholder="Search schema..." />
+              <CommandList>
+                <CommandEmpty>No schema found.</CommandEmpty>
+                <CommandGroup>
+                  {schemas.map((item) => (
+                    <CommandItem
+                      key={item.schema_name}
+                      value={item.schema_name}
+                      onSelect={(currentValue) => {
+                        setSchema(currentValue === schema ? "" : currentValue);
+                        setOpen(false);
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          "mr-2 h-4 w-4",
+                          schema === item.schema_name
+                            ? "opacity-100"
+                            : "opacity-0"
+                        )}
+                      />
+                      {item.schema_name}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </>
+      )}
+    </Popover>
+  );
+}
+
+function DatabaseTable({
+  table,
+  loading,
+  schema,
+  schemas,
+}: {
+  table: any;
+  loading: boolean;
+  schema: string;
+  schemas: { schema_name: string }[];
+}) {
+  return (
+    <Table>
+      <TableHeader>
+        {table
+          .getHeaderGroups()
+          .map(
+            (headerGroup: { id: Key | null | undefined; headers: any[] }) => (
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => {
                   return (
@@ -301,20 +352,27 @@ export default function TablesPage() {
                   );
                 })}
               </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {loading ? (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="space-y-2">
-                  <Skeleton className="w-full h-[20px] rounded-md" />
-                  <Skeleton className="w-full h-[20px] rounded-md" />
-                </TableCell>
-              </TableRow>
-            ) : (
-              <>
-                {table.getRowModel()?.rows?.length ? (
-                  table.getRowModel().rows.map((row) => (
+            )
+          )}
+      </TableHeader>
+      <TableBody>
+        {loading ? (
+          <TableRow>
+            <TableCell colSpan={columns.length} className="space-y-2">
+              <Skeleton className="w-full h-[20px] rounded-md" />
+              <Skeleton className="w-full h-[20px] rounded-md" />
+            </TableCell>
+          </TableRow>
+        ) : (
+          <>
+            {table.getRowModel()?.rows?.length ? (
+              table
+                .getRowModel()
+                .rows.map(
+                  (row: {
+                    id: Key | null | undefined;
+                    getVisibleCells: () => any[];
+                  }) => (
                     <TableRow key={row.id}>
                       {row.getVisibleCells().map((cell) => (
                         <TableCell key={cell.id}>
@@ -325,29 +383,28 @@ export default function TablesPage() {
                         </TableCell>
                       ))}
                     </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell
-                      colSpan={columns.length}
-                      className="h-24 text-center"
-                    >
-                      No tables found in{" "}
-                      <span className="font-semibold">
-                        {
-                          schemas.find((item) => item.schema_name === schema)
-                            ?.schema_name
-                        }
-                      </span>{" "}
-                      schema
-                    </TableCell>
-                  </TableRow>
-                )}
-              </>
+                  )
+                )
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No tables found in{" "}
+                  <span className="font-semibold">
+                    {
+                      schemas.find((item) => item.schema_name === schema)
+                        ?.schema_name
+                    }
+                  </span>{" "}
+                  schema
+                </TableCell>
+              </TableRow>
             )}
-          </TableBody>
-        </Table>
-      </div>
-    </div>
+          </>
+        )}
+      </TableBody>
+    </Table>
   );
 }
