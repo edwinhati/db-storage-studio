466d0476d3cd7e88e4d1ace97f1925df4fbe6fd9
edwinhati <edwinhati@outlook.com>
2024-08-08 00:28:01 +0700

    Merge branch 'boilerplate' into DEV_PG

--- src/components/navigations/sidebar/sidebar.tsx
+++ src/components/navigations/sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import Link from "next/link";
-import { ModeToggle } from "../utils/mode-toggle";
+import { ModeToggle } from "@/components/utils/mode-toggle";
 import { Package2, Settings, LucideProps } from "lucide-react";
 
 import {
@@ -27,7 +27,7 @@ function Sidebar({ navigation }: NavigationProps) {
         <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
           <Link
             href="#"
-            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-md font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+            className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
           >
             <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
             <span className="sr-only">Acme Inc</span>
